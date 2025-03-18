eb0e4fbd4d265a28d389e57343336f3a37f89ef5
MarkWang <kugouming@sina.com>
2025-03-18 19:06:53 +0800

    update

--- examples/pingo-hello-world/main.go
+++ examples/pingo-hello-world/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/dullgiulio/pingo"
+
+	"github.com/kugouming/pingo"
 )
 
 type Plugin struct{}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kugouming/pingo
+
+go 1.23.0
